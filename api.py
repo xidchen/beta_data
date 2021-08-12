122693583eef37b9e28014eb0439e85f912902cd
xdchen <xdchen@betawm.com>
2021-08-12 10:59:38 +0800

    Update intent model

--- api.py
+++ api.py
@@ -78,7 +78,7 @@ def main():
 
 if __name__ == '__main__':
     intent_class_path = './intent_classes.txt'
-    intent_model_path = './beta_bert_intent_l323_t7000_e6_f85_sm'
+    intent_model_path = './beta_bert_intent_l357_t9000_e6_f84_sm'
     entity_class_path = './entity_classes.txt'
     entity_model_path = './beta_bert_entity_l7_t920_e3_bio_s1_h5'
     bert_model_config = 'bert_zh_L-12_H-768_A-12/3'
--- beta_utils.py
+++ beta_utils.py
@@ -1,18 +1,14 @@
-import re
-
-
 def replace_token_for_bert(_text: str) -> str:
+    """
+    Replace token for BERT-zh tokenization and standardization
+    """
     replace_dict = {'“': '"',
                     '”': '"',
                     '‘': '\'',
                     '’': '\'',
+                    '（': '(',
+                    '）': ')',
                     '—': '-'}
     for k, v in replace_dict.items():
         _text = _text.replace(k, v)
     return _text.lower()
-
-
-def replace_whitespace_in_pattern(_text: str) -> str:
-    if re.fullmatch(r'(\d{6}\s+)+\d{6}', _text):
-        return re.sub(r'\s+', '，', _text)
-    return _text
