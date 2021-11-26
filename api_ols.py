811f9bc1f063250e0660984885f95eba1d8c608e
xdchen <xdchen@betawm.com>
2021-11-26 09:55:17 +0800

    Optimize api_ols.py

--- api_ols.py
+++ api_ols.py
@@ -1,8 +1,7 @@
 import flask
 import tensorflow as tf
 
-physical_devices = tf.config.list_physical_devices('GPU')
-for device in physical_devices:
+for device in tf.config.list_physical_devices('GPU'):
     tf.config.experimental.set_memory_growth(device, True)
 tf.get_logger().setLevel('ERROR')
 
@@ -35,4 +34,5 @@ def main():
 
 
 if __name__ == '__main__':
+    tf.linalg.inv(tf.random.normal([2, 2]))
     app.run('0.0.0.0', 5200)
