c3a3566f7f62bb501b3212cdd9598e135bb59988
xdchen <xdchen@betawm.com>
2022-01-18 16:13:59 +0800

    Add run_baidu_ocr in simple_ocr.py

--- simple_ocr.py
+++ simple_ocr.py
@@ -1,16 +1,47 @@
+import base64
 import os
 import PIL.Image
 import pytesseract
+import requests
 
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 data_root_path = os.path.join(root_dir, 'BetaData', 'ocr', 'fig')
 
-for image in sorted(os.listdir(data_root_path)):
-    im = PIL.Image.open(os.path.join(data_root_path, image))
-    print(image, im.mode, im.size, im.info)
-    im = im.resize(im.size, PIL.Image.LANCZOS)
-    s = pytesseract.image_to_string(im, lang='chi_sim', config='--dpi 300')
-    while ' ' in s or '\n\n' in s:
-        s = s.replace(' ', '').replace('\n\n', '\n')
-    print(s)
+
+def run_ocr(data_dir: str):
+    """Run OCR on image files in a directory"""
+    for image in sorted(os.listdir(data_dir)):
+        im = PIL.Image.open(os.path.join(data_dir, image))
+        print(image, im.mode, im.size, im.info)
+        im = im.resize(im.size, PIL.Image.LANCZOS)
+        s = pytesseract.image_to_string(im, lang='chi_sim', config='--dpi 300')
+        while ' ' in s or '\n\n' in s:
+            s = s.replace(' ', '').replace('\n\n', '\n')
+        print(s)
+
+
+def run_baidu_ocr(data_dir: str):
+    """Run Baidu OCR on image files in a directory"""
+    url = 'https://aip.baidubce.com/oauth/2.0/token'
+    params = {'grant_type': 'client_credentials',
+              'client_id': 'YAdlb1AZWdHxX9mRs5S7Wgny',
+              'client_secret': 'IMVSe74vmEqGdmEYEhIcgIww3m4il9Rh'}
+    response = requests.get(url, params=params)
+    access_token = response.json()['access_token']
+    url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic'
+    params = {'access_token': access_token}
+    for image in sorted(os.listdir(data_dir)):
+        im = PIL.Image.open(os.path.join(data_dir, image))
+        print(image, im.mode, im.size, im.info)
+        f = open(os.path.join(data_dir, image), 'rb')
+        im = base64.b64encode(f.read())
+        data = {'image': im}
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        response = requests.post(url, data=data, params=params, headers=headers)
+        rs = response.json()
+        s = '\n'.join([r['words'] for r in rs['words_result']]) + '\n'
+        print(s)
+
+
+run_ocr(data_root_path)
