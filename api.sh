e7f7b12f6e3d556d85f1728817293f8b9c5a1db2
xdchen <xdchen@betawm.com>
2022-02-22 18:28:42 +0800

    Update api.sh

--- api.sh
+++ api.sh
@@ -5,3 +5,4 @@ nohup python -u api_ols.py > api_ols.out 2>&1 &
 nohup python -u api_useml.py > api_useml.out 2>&1 &
 nohup python -u api_bert_tokenizer.py > api_bert_tokenizer.out 2>&1 &
 nohup python -u api_ocr.py > api_ocr.out 2>&1 &
+nohup python -u api_coach.py > api_coach.out 2>&1 &
