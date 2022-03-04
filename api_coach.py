bc3161f5132e79a0dcb9f7fdef29c76eb546384c
xdchen <xdchen@betawm.com>
2022-03-04 10:07:33 +0800

    Update api_coach.py

--- api_coach.py
+++ api_coach.py
@@ -4,6 +4,7 @@ import random
 import werkzeug.utils
 
 import beta_audio
+import beta_coach
 
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
@@ -26,20 +27,27 @@ def main():
         audio_file_path = os.path.join(upload_files_dir, audio_file)
         res_file_path = os.path.join(result_files_dir, res_file)
         a.save(audio_file_path)
-        # keywords = k.split(sep=';')
-        res = None
         asr_res = beta_audio.run_bidu_asr(audio_file_path)
         transcript = asr_res['result'][0] if 'result' in asr_res else None
-        with open(res_file_path, 'w', encoding='utf-8') as f:
-            f.write(str(transcript))
-        default = {'transcript': transcript,
-                   'scores': {'rhetoric': random.randint(10, 99),
-                              'keywords': random.randint(10, 99),
+        if transcript:
+            with open(res_file_path, 'w', encoding='utf-8') as f:
+                f.write(str(transcript))
+            rhetoric_score = beta_coach.rhetoric_score(r, transcript)
+            keywords_score = beta_coach.keywords_score(k, transcript)
+            speed_score = beta_coach.speed_score(audio_file_path, transcript)
+            res = {'transcript': transcript,
+                   'scores': {'rhetoric': rhetoric_score,
+                              'keywords': keywords_score,
                               'fluency': random.randint(10, 99),
                               'articulation': random.randint(10, 99),
-                              'speed': random.randint(10, 99)}}
-        if not res:
-            res = default
+                              'speed': speed_score}}
+        else:
+            res = {'transcript': transcript,
+                   'scores': {'rhetoric': 0,
+                              'keywords': 0,
+                              'fluency': 0,
+                              'articulation': 0,
+                              'speed': 0}}
         return flask.jsonify(res)
     else:
         res = {'error_msg': 'input error'}
