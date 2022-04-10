eaa5f591429ea7437cf5d01850aa0918ca47af90
xdchen <xdchen@betawm.com>
2022-04-10 16:09:17 +0800

    Update api_coach.py by adding two parameters, urls and transcripts

--- api_coach.py
+++ api_coach.py
@@ -1,6 +1,6 @@
 import flask
 import os
-import werkzeug.utils
+import werkzeug.utils as wu
 
 import beta_audio
 import beta_coach
@@ -16,22 +16,29 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main():
-    u = flask.request.form.get('url')
+    v = flask.request.form.get('url')
+    u = flask.request.form.get('urls')
     r = flask.request.form.get('rhetoric')
     k = flask.request.form.get('keywords')
-    t = flask.request.form.get('transcript')
-    print(f'U: {u}')
-    print(f'R: {r}')
-    print(f'K: {k}')
-    print(f'T: {t}')
+    s = flask.request.form.get('transcript')
+    t = flask.request.form.get('transcripts')
     if u and r and k and t:
-        audio_file = u.rsplit('/', 1)[-1]
-        audio_file = werkzeug.utils.secure_filename(audio_file)
-        amr_file_path = beta_audio.get_amr_audio(u, audio_file, upload_dir)
-        wav_file_path = beta_audio.convert_amr_to_wav(amr_file_path)
+        try:
+            u, t = eval(u), eval(t)
+            assert isinstance(u, list) is True
+            assert isinstance(t, list) is True
+        except (NameError, SyntaxError, AssertionError):
+            res = {'error_msg': 'input error'}
+            return flask.jsonify(res)
+        print(f'U: {u}')
+        print(f'R: {r}')
+        print(f'K: {k}')
+        print(f'T: {t}')
+        wav_file_path = beta_audio.get_wav_from_amr_urls(u, upload_dir)
         transcript_path = beta_audio.replace_ext_to_txt(wav_file_path)
+        t = ''.join(t)
         with open(transcript_path, 'w', encoding='utf-8') as f:
-            f.write(str(t))
+            f.write(t)
         duration = beta_audio.get_duration(wav_file_path)
         rhetoric_score = beta_coach.rhetoric_score(r, t)
         keywords_score = beta_coach.keywords_score(k, t)
@@ -44,6 +51,32 @@ def main():
                           'fluency': fluency_score,
                           'articulation': articulation_score},
                'transcript': t}
+        print(res['scores'])
+        return flask.jsonify(res)
+    elif v and r and k and s:
+        print(f'V: {v}')
+        print(f'R: {r}')
+        print(f'K: {k}')
+        print(f'S: {s}')
+        audio_file = v.rsplit('/', 1)[-1]
+        audio_file = wu.secure_filename(audio_file)
+        amr_file_path = beta_audio.get_amr_audio(v, audio_file, upload_dir)
+        wav_file_path = beta_audio.convert_amr_to_wav(amr_file_path)
+        transcript_path = beta_audio.replace_ext_to_txt(wav_file_path)
+        with open(transcript_path, 'w', encoding='utf-8') as f:
+            f.write(str(s))
+        duration = beta_audio.get_duration(wav_file_path)
+        rhetoric_score = beta_coach.rhetoric_score(r, s)
+        keywords_score = beta_coach.keywords_score(k, s)
+        speed_score = beta_coach.speed_score(duration, s)
+        fluency_score = beta_coach.fluency_score(duration, wav_file_path)
+        articulation_score = beta_coach.articulation_score(wav_file_path)
+        res = {'scores': {'rhetoric': rhetoric_score,
+                          'keywords': keywords_score,
+                          'speed': speed_score,
+                          'fluency': fluency_score,
+                          'articulation': articulation_score},
+               'transcript': s}
         print(res['scores'])
         return flask.jsonify(res)
     else:
