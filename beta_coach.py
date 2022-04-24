d937cd7719ff44ac35b0017908cce5de241d693b
xdchen <xdchen@betawm.com>
2022-04-24 21:56:30 +0800

    Update beta_coach.py by accelerating computation

--- beta_coach.py
+++ beta_coach.py
@@ -1,41 +1,32 @@
 import json
-import librosa
 import numpy as np
 import random
 import re
 import requests
-import tensorflow as tf
 import zlib
 
 
-def semantic_similarity(s1: str, s2: str) -> float:
-    """Calculate semantic similarity of two sentences
-    :param s1: first sentence
-    :param s2: second sentence
-    :return: semantic similarity
+def semantic_embedding(s0: [str]) -> [[float]]:
+    """Calculate semantic embedding of a list of sentences
+    :param s0: list of sentences
+    :return: list of semantic embeddings
     """
     url = 'http://localhost:5300'
-    res = 0
-    if s1 and s2:
-        r = json.loads(requests.post(url, data={'s1': s1, 's2': s2}).text)
-        r1 = tf.reshape(tf.constant(r['s1_embed']), shape=[1, 512])
-        r2 = tf.reshape(tf.constant(r['s2_embed']), shape=[1, 512])
-        res = tf.einsum('ij,kj->ik', r1, r2).numpy().tolist()[0][0]
-    return res
+    r = json.loads(requests.post(url, data={'s0': str(s0)}).text)
+    return r['s0_embed']
 
 
-def split_sentence(sentence: str) -> []:
+def split_sentence(sentence: str) -> [str]:
     """Split sentence at given punctuations into list of phrases
     :param sentence: the sentence
     :return: list of phrases
     """
     punctuations = ['。', '！', '？', '，', ';', '：']
     pattern = '[' + ''.join(punctuations) + ']'
-    res = re.split(pattern, sentence)
-    return res
+    return [_s.strip() for _s in re.split(pattern, sentence) if _s.strip()]
 
 
-def split_by_semicolon(s: str) -> []:
+def split_by_semicolon(s: str) -> [str]:
     """Split a string by semicolon into list of strings
     :param s: the string
     :return: list of strings
@@ -65,7 +56,8 @@ def rhetoric_score(rhetoric: str, transcript: str) -> int:
     """
     rhetoric = drop_ending_punctuation(rhetoric)
     transcript = drop_ending_punctuation(transcript)
-    score = semantic_similarity(rhetoric, transcript)
+    se = semantic_embedding([rhetoric, transcript])
+    score = np.inner(se[0], se[1])
     res = int(score * 100) if score >= 0 else 0
     return res
 
@@ -76,18 +68,14 @@ def keywords_score(keywords: str, transcript: str) -> int:
     :param transcript: user's ASR result
     :return: score
     """
-    res = 0
-    if keywords and transcript:
-        keywords = split_by_semicolon(keywords)
-        transcripts = split_sentence(transcript)
-        scores = []
-        for k in keywords:
-            _scores = []
-            for t in transcripts:
-                _score = 1 if k in t else semantic_similarity(k, t)
-                _scores.append(_score)
-            scores.append(np.max(_scores))
-        res = int(np.mean(scores) * 100)
+    keywords = split_by_semicolon(keywords)
+    transcripts = split_sentence(transcript)
+    se = semantic_embedding(keywords + transcripts)
+    scores = np.inner(se[:len(keywords)], se[len(keywords):])
+    k_in_t = np.array([[1] if k in transcript else [0] for k in keywords])
+    scores = np.hstack((scores, k_in_t))
+    scores = np.max(scores, axis=1)
+    res = int(np.mean(scores) * 100)
     return res
 
 
@@ -98,12 +86,11 @@ def speed_score(duration: float, transcript: str) -> int:
     :return: score
     """
     res = 0
-    if duration and transcript:
-        speed = int(len(transcript) / duration)
-        if 1 <= speed < 2 or 5 <= speed < 6:
-            res = 60
-        if 2 <= speed < 5:
-            res = 100
+    speed = int(len(transcript) / duration)
+    if 1 <= speed < 2 or 5 <= speed < 6:
+        res = 60
+    if 2 <= speed < 5:
+        res = 100
     return res
 
 
