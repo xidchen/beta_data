a1b10dced901e8e3f0e86a3f709df3c2fe20935a
xdchen <xdchen@betawm.com>
2022-04-24 21:55:08 +0800

    Update api_useml.py

--- api_useml.py
+++ api_useml.py
@@ -13,49 +13,40 @@ tf.get_logger().setLevel('ERROR')
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def main():
-    s1, s2, lb, res = '', '', '', {}
-    if flask.request.method == 'GET':
-        s1 = flask.request.args.get('s1', '')
-        s2 = flask.request.args.get('s2', '')
-        lb = flask.request.args.get('lb', '')
-    if flask.request.method == 'POST':
-        s1 = flask.request.form.get('s1', '')
-        s2 = flask.request.form.get('s2', '')
-        lb = flask.request.form.get('lb', '')
-    print(f'S1: {s1}')
-    print(f'S2: {s2}')
-    if lb:
+    s0 = flask.request.form.get('s0', '')
+    s1 = flask.request.form.get('s1', '')
+    s2 = flask.request.form.get('s2', '')
+    if s0:
         try:
-            lb = eval(lb)
-            assert isinstance(lb, list) is True
-            print(f'Label {len(lb)}: {lb}')
-            print('S1 and S2 will not be considered.')
-            res['status'] = '200 OK'
-            res['l_embed'] = useml_embed(lb).numpy().tolist()
+            s0 = eval(s0)
+            assert isinstance(s0, list) is True
+            print(f'S0 (len: {len(s0)}): {s0}')
+            res = {'s0_embed': useml(s0).numpy().tolist()}
         except (NameError, SyntaxError):
-            print(f'Label parsing error: {lb}')
-            res['status'] = '400 Bad Request'
-        return res
-    ml = 1000
+            print(f'S0 parsing error: {s0}')
+            res = {'error_msg': 'input error'}
+        return flask.jsonify(res)
     if s1 or s2:
-        res['status'] = '200 OK'
+        ml = 1000
+        print(f'S1: {s1}')
+        print(f'S2: {s2}')
         s1 = beta_utils.split_one_line_long_article(s1, ml) if s1 else []
         s2 = beta_utils.split_one_line_long_article(s2, ml) if s2 else []
         q = s1 + s2 if s1 and s2 else s1 if s1 else s2
-        q_embed = useml_embed(q)
-        res['s1_embed'] = tf.reduce_mean(
-            q_embed[:len(s1)], 0).numpy().tolist() if s1 else []
-        res['s2_embed'] = tf.reduce_mean(
-            q_embed[len(s1):], 0).numpy().tolist() if s2 else []
+        q_embed = useml(q)
+        res = {'s1_embed': tf.reduce_mean(
+            q_embed[:len(s1)], 0).numpy().tolist() if s1 else [],
+               's2_embed': tf.reduce_mean(
+            q_embed[len(s1):], 0).numpy().tolist() if s2 else []}
     else:
-        res['status'] = '400 Bad Request'
-        res['s1_embed'] = res['s2_embed'] = []
+        res = {'error_msg': 'input error'}
     return flask.jsonify(res)
 
 
 if __name__ == '__main__':
-    useml_embed = hub.load("https://tfhub.dev/google/"
-                           "universal-sentence-encoder-multilingual-large/3")
+    useml = hub.load('https://tfhub.dev/google/'
+                     'universal-sentence-encoder-multilingual-large/3')
+    useml(['0'])
     app.run('0.0.0.0', 5300)
