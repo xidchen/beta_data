48bd7c6079e43f3962bd3607730ecbb9889b241c
xdchen <xdchen@betawm.com>
2022-05-22 13:06:43 +0800

    Update api.sh by using gunicorn

--- api.sh
+++ api.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-nohup python -u api.py > api.out 2>&1 &
+nohup gunicorn -w 2 -b 0:5000 api:app --access-logfile - > api.out 2>&1 &
 nohup python -u api_chatbot.py > api_chatbot.out 2>&1 &
 nohup python -u api_ols.py > api_ols.out 2>&1 &
-nohup python -u api_useml.py > api_useml.out 2>&1 &
+nohup gunicorn -w 2 -b 0:5300 api_useml:app --access-logfile - > api_useml.out 2>&1 &
 nohup python -u api_bert_tokenizer.py > api_bert_tokenizer.out 2>&1 &
 nohup python -u api_ocr.py > api_ocr.out 2>&1 &
-nohup python -u api_coach.py > api_coach.out 2>&1 &
-nohup python -u api_fluency.py > api_fluency.out 2>&1 &
+nohup gunicorn -w 2 -b 0:5600 api_coach:app --access-logfile - > api_coach.out 2>&1 &
+nohup gunicorn -w 2 -b 0:5610 api_fluency:app --access-logfile - > api_fluency.out 2>&1 &
