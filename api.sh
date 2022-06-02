dbf48c9e82b428c5d57447b8878814aa1d6a25b1
xdchen <xdchen@betawm.com>
2022-06-02 14:52:51 +0800

    Update api.sh on api_articulation

--- api.sh
+++ api.sh
@@ -7,3 +7,4 @@ nohup python -u api_bert_tokenizer.py > api_bert_tokenizer.out 2>&1 &
 nohup python -u api_ocr.py > api_ocr.out 2>&1 &
 nohup gunicorn -w 2 -b 0:5600 api_coach:app --access-logfile - > api_coach.out 2>&1 &
 nohup gunicorn -w 2 -b 0:5610 api_fluency:app --access-logfile - > api_fluency.out 2>&1 &
+nohup gunicorn -w 2 -b 0:5620 api_articulation:app --access-logfile - > api_articulation.out 2>&1 &
