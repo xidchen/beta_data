965d634911701f8afdfe59d4c55773a5fc6b08b6
xdchen <xdchen@betawm.com>
2022-08-16 10:49:48 +0800

    Update api_useml.py by normalizing vectors

--- api_useml.py
+++ api_useml.py
@@ -10,6 +10,8 @@ for device in tf.config.list_physical_devices('GPU'):
 tf.get_logger().setLevel('ERROR')
 
 
+MAX_LEN = 1000
+
 useml = hub.load('https://tfhub.dev/google/'
                  'universal-sentence-encoder-multilingual-large/3')
 useml(['0'])
@@ -25,28 +27,27 @@ def main():
     s2 = flask.request.form.get('s2', '')
     if s0:
         try:
-            s0 = eval(s0.replace('\r', '').replace('\n', '').strip())
+            s0 = eval(s0)
             assert isinstance(s0, list) is True
             print(f'S0 (len: {len(s0)}): {s0}')
             res = {'s0_embed': useml(s0).numpy().tolist()}
-        except (NameError, SyntaxError):
-            print(f'S0 parsing error: {s0}')
+        except (NameError, SyntaxError) as e:
+            print(f'S0 parsing error ({e}): {s0}')
             res = {'error_msg': 'input error'}
         return flask.jsonify(res)
     if s1 or s2:
-        ml = 1000
-        s1 = s1.replace('\r', '').replace('\n', '').strip()
-        s2 = s2.replace('\r', '').replace('\n', '').strip()
-        print(f'S1: {s1}')
-        print(f'S2: {s2}')
-        s1 = beta_utils.split_one_line_long_article(s1, ml) if s1 else []
-        s2 = beta_utils.split_one_line_long_article(s2, ml) if s2 else []
+        print(f'S1: {s1}\nS2: {s2}')
+        s1 = beta_utils.split_one_line_long_article(s1, MAX_LEN) if s1 else []
+        s2 = beta_utils.split_one_line_long_article(s2, MAX_LEN) if s2 else []
         q = s1 + s2 if s1 and s2 else s1 if s1 else s2
         q_embed = useml(q)
-        res = {'s1_embed': tf.reduce_mean(
-            q_embed[:len(s1)], 0).numpy().tolist() if s1 else [],
-               's2_embed': tf.reduce_mean(
-            q_embed[len(s1):], 0).numpy().tolist() if s2 else []}
+        s1_embed = tf.linalg.normalize(tf.reshape(
+            tf.reduce_mean(q_embed[:len(s1)], 0),
+            shape=[1, 512]))[0].numpy().tolist() if s1 else []
+        s2_embed = tf.linalg.normalize(tf.reshape(
+            tf.reduce_mean(q_embed[len(s1):], 0),
+            shape=[1, 512]))[0].numpy().tolist() if s2 else []
+        res = {'s1_embed': s1_embed, 's2_embed': s2_embed}
     else:
         res = {'error_msg': 'input error'}
     return flask.jsonify(res)
