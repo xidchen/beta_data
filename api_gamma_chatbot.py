fb95ca11182aa9f2259b712f0d3e89fee3e47d65
xdchen <xdchen@betawm.com>
2022-10-16 22:47:50 +0800

    Update api_gamma_chatbot.py by supporting hot deploying new data file

--- api_gamma_chatbot.py
+++ api_gamma_chatbot.py
@@ -18,9 +18,7 @@ default_t = .8
 
 df = gamma_chatbot.load_excel(file_path, sheet_name)
 df2 = gamma_chatbot.extract_kt_and_sq(df)
-se = gamma_chatbot.run_sentence_encoder_on_df(df2)
-
-
+se = gamma_chatbot.run_sentence_encoder_on_df(df2, False)
 md_dict = {}
 
 
@@ -29,26 +27,46 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main():
+    global df, df2, se, md_dict
     q = flask.request.form.get('q', '')
     p = flask.request.form.get('p', '')
     t = flask.request.form.get('t', '')
+    x = flask.request.files.get('x')
+    res = {}
+    if x:
+        if gamma_chatbot.allowed_file(x.filename):
+            x_path = gamma_chatbot.saved_excel_path(x, data_root_path)
+            m = {'x': x.filename, 'x_path': x_path}
+            print(m)
+            df = gamma_chatbot.load_excel(x_path, sheet_name)
+            df2 = gamma_chatbot.extract_kt_and_sq(df)
+            se = gamma_chatbot.run_sentence_encoder_on_df(df2, True)
+            md_dict = {}
+            res['status'] = '200 OK'
+            print(res)
+            return flask.jsonify(res)
+        else:
+            m = {'x': x.filename}
+            print(m)
+            res['status'] = '400 Bad Request'
+            print(res)
+            return flask.jsonify(res)
     p = p if p else default_p
     t = t if t else default_t
-    d = {'q': q, 'p': p, 't': t}
-    print(d)
-    md = hashlib.md5(str(d).encode('utf-8')).hexdigest()
+    m = {'q': q, 'p': p, 't': t}
+    print(m)
+    md = hashlib.md5(str(m).encode('utf-8')).hexdigest()
     if md in md_dict:
         res = md_dict[md]
         print(res)
         return flask.jsonify(res)
-    res = {}
     try:
         t = float(t)
     except ValueError:
         res['status'] = '400 Bad Request'
         print(res)
         return flask.jsonify(res)
-    if q and 0 <= t <= 1:
+    if q and 0 < t <= 1:
         qe = gamma_chatbot.run_sentence_encoder_on_str(q)
         ss = gamma_chatbot.calculate_similarity_scores(se, qe)
         top_kt = gamma_chatbot.collect_top_kt(df2, ss, t)
