996a01b9089844299e1450778336d5e13aff4953
xidchen <xidchen.work@gmail.com>
2022-11-26 06:08:21 +0800

    Update alpha_chatbot.py on sleep time

--- alpha_chatbot.py
+++ alpha_chatbot.py
@@ -40,7 +40,7 @@ def run_sentence_encoder_on_df(df: pd.DataFrame, hd: bool) -> [[float]]:
     :param hd: whether it is a hot deployment
     :return: the sentence embeddings
     """
-    time.sleep(0) if hd else time.sleep(16)
+    time.sleep(0) if hd else time.sleep(20)
     return run_sentence_encoder(df['sq'].tolist())
 
 
